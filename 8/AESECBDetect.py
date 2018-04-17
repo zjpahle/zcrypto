e608f1f4b30fb3189393974b79f4205ab6370383
zjpahle <zjpahle@users.noreply.github.com>
2018-04-17 16:12:38 -0700

    completed challenge 8

--- 8/AESECBDetect.py
+++ 8/AESECBDetect.py
@@ -1,21 +1,34 @@
 import os
-import base64
-from .. import zcrypto
-from Crypto.Cipher import AES
+#import base64
+import zcrypto
+from zcrypto import numpy as np
+import matplotlib.pyplot as mpl
+#from Crypto.Cipher import AES
 #unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 fileName = '8.txt'
 fileOutName = '8out.txt'
 cwd = os.getcwd()
 
+############
+#read in AES encrypted data
 with open(cwd + '\\' +fileName, 'r') as cipher_data:
-    cipher_text = base64.b64decode(cipher_data.read())
-#print(type(cipher_text))
-#print(cipher_text)
+    cipherText = cipher_data.read().split()
     
-decryption_suite = AES.new('YELLOW SUBMARINE', AES.MODE_ECB)
-plain_text = decryption_suite.decrypt(cipher_text)
-
-
-with open(cwd + '\\' + fileOutName, 'wb') as out_text:
-    out_text.write(plain_text)
+for line in range(len(cipherText)):
+#for line in range(10):
+    hd = []
+    for kgLen in range(2,int(len(cipherText[line])/2)):
+        #   print('At key length '+ str(kgLen) + ' the hamm dist is: '+ str(hd[kgLen-1]))
+        chunkList = []
+        for chunk in range(1,int(len(cipherText[line])/kgLen)-1):
+            a = bytearray(cipherText[line][chunk*kgLen:(chunk+1)*kgLen], encoding = "utf-8")
+            b = bytearray(cipherText[line][(chunk+1)*kgLen:(chunk+2)*kgLen], encoding = "utf-8")
+            chunkList.append(zcrypto.hammingDistBin(a, b))
+        avg = np.mean(chunkList)
+        hd.append(avg/kgLen)
+    
+    mpl.figure(line)
+    hdArray =  np.array(hd)
+    keyLen = np.arange(1,int(len(cipherText[line])/2)-1)
+    mpl.plot(keyLen, hdArray)
--- breakRepeatingXor.py
+++ breakRepeatingXor.py
@@ -23,10 +23,10 @@ cipherText = bytearray(fileIn.read().decode('base64'))
 keysizeGuess = []
 
 #take the hamming distance of the keysize guesses. Smallest dist is key length
-for keysize in xrange(2,41):
+for keysize in range(2,41):
 	keysizeGuess.append(zcrypto.hammingDistBin(cipherText[0:keysize],cipherText[keysize:2*keysize])/keysize)
 keysize = []
-for x in xrange(5):
+for x in range(5):
 	#print keysizeGuess
 	keysize.append(keysizeGuess.index(min(keysizeGuess)) + 2)
 	keysizeGuess[keysize[x]-2] = 255
@@ -35,28 +35,28 @@ keysize = [22]
 
 keyGuess = []
 #for each possible key length:
-for keyLenCnt in xrange(len(keysize)):
+for keyLenCnt in range(len(keysize)):
 	#create dictionary for cipher blocks
-	block = {x:[] for x in xrange(keysize[keyLenCnt])}
+	block = {x:[] for x in range(keysize[keyLenCnt])}
 
 	#break ciphertext into blocks by individual key characters
-	for x in xrange(len(block)):
+	for x in range(len(block)):
 		block[x] = cipherText[x::keysize[keyLenCnt]]
 
-	plainTxtBlks = {x:[] for x in xrange(keysize[keyLenCnt])}
+	plainTxtBlks = {x:[] for x in range(keysize[keyLenCnt])}
 
 	#for each block:
-	for blockCount in xrange(keysize[keyLenCnt]):
+	for blockCount in range(keysize[keyLenCnt]):
 		guesses = [] #reset the guesses each time
 		#for each single char key guess:
-		for keyCount in xrange(256):
+		for keyCount in range(256):
 			thisGuess = zcrypto.singleChrXor(bytearray(chr(keyCount)),block[blockCount])
 			guesses.append([zcrypto.checkTruth(english, thisGuess), chr(keyCount), str(thisGuess)]) #[rank, key, guessTxt]
 			guesses = sorted(guesses, key=zcrypto.getKey, reverse=True)
-		# for x in xrange(0,len(guesses)): #debugging for
+		# for x in range(0,len(guesses)): #debugging for
 		# 	testFileOut.write(str(guesses[x]) + '\n')
 		plainTxtBlks[blockCount] = guesses[0] #best guess get placed in dict
-	keyGuess.append(''.join([plainTxtBlks[blockCount][1] for blockCount in xrange(len(plainTxtBlks))]))
+	keyGuess.append(''.join([plainTxtBlks[blockCount][1] for blockCount in range(len(plainTxtBlks))]))
 	#print keyGuess
 	print 'the best guess key of length ' + str(keysize[keyLenCnt]) + ' is: ' + str(keyGuess[keyLenCnt])
 
